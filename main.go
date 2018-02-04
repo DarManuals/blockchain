b1ef090f399dea5b8ee51961c39a5de89c0b8f05
Daragan <darbodya@gmail.com>
2018-02-04 15:10:07 +0200

    Endpoint is changed.

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 	router.HandleFunc("/blockchain/receive_update", controllers.Update).Methods("POST")
 	router.HandleFunc("/management/add_transaction", controllers.AddTransaction).Methods("POST")
 	router.HandleFunc("/management/add_link", controllers.AddLink).Methods("POST")
-	router.HandleFunc("/management/status", controllers.GetStatus).Methods("GET")
+	router.HandleFunc("/management/state", controllers.GetStatus).Methods("GET")
 	router.HandleFunc("/management/sync", controllers.Sync).Methods("GET")
 
 	http.ListenAndServe(":3000", router)
